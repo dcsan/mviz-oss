8577cd45457add8a0b4c1d3e2f307fd66278ba39
dcsan <dc@klab.com>
2013-09-27 17:36:02 -0700

    minimal queries

--- lib/queries.py
+++ lib/queries.py
@@ -3,119 +3,23 @@
 
 from datetime import datetime, timedelta
 
-tutorial_fields = {
-    "player:uid": 1, 
-    "account:age:seconds": 1, 
-    "step": 1, 
-    'event':1,
-    'EventMajor':1,
-    'EventMinor':1,
-    'EventData':1
-    }
-
 tstoday = {"$gte": datetime.utcnow()-timedelta(days=1) }
 tsweek  = {"$gte": datetime.utcnow()-timedelta(days=7) }
-
-tutorial_match_day  = {'event': 'Tutorial', 'ts': tstoday }
 
 query_list = {
 
     'test': {
         'desc': "test query",
-        'q':  {'event': 'Tutorial'},
+        'q':  {'ts': tstoday },
         'proj': {"player:uid":1, "ts":1 }
     },
-
-    'tutorial': {
-        'desc': "Tutorial query",
-        'q':  {'event': 'Tutorial'},
-        'proj': tutorial_fields
-    },
-
-    'tutorial-day': {
-        'desc': "Daily Tutorial query",
-        'q':  tutorial_match_day,
-        'proj': tutorial_fields
-    },
-
-    'tutorial-day-all': {
-        'desc': "Daily Tutorial query with all fields",
-        'q':  tutorial_match_day,
-        'proj': {}
-    },
-
-    'tutorial-day-min': {
-        'desc': "Daily Tutorial query with minimal fields",
-        'q':  tutorial_match_day,
-        'proj': {
-            # "step": 1, 
-            'event':1,
-            'EventMajor':1,
-            'EventMinor':1,
-            'EventData':1
-        }
-
-    },
-
-    'tutorial-week': {
-        'desc': "Weekly Tutorial query",
-        'q':  {'event': 'Tutorial', 'ts': tsweek },
-        'proj': tutorial_fields
-    },
-
-    'tutorial-all': {
-        'desc': "All of the tutorials",
-        'q'     :  {'event': 'Tutorial'},
-        'proj'  : {},
-        'limit' : 5,
-        'sort'  : ['ts', -1]
-    },
-
-    'register-day': {
-        'desc': "Tutorial Test",
-        'q'     :  {'event': 'register', 'ts': tstoday },
-        'proj'  : {},
-        'limit' : 5,
-        'sort'  : ['ts', -1]
-    },
-
-    'login-day': {
-        'desc': "Tutorial Test",
-        'q'     :  {'event': 'login', 'ts': tstoday },
-        'proj'  : {},
-        'limit' : 5,
-        'sort'  : ['ts', -1]
-    },
-
-    'cashflow': {
-        'desc': "What users are buying?",
-        'q'     :  {'event': 'cashflow'},
-        'proj'  : {"type": 1, "amount": 1, "ts":1, "player:id":1 },
-        'limit' : 5,
-        'sort'  : ['ts', -1]
-    },
-
-    'boost-day': {
-        'desc': "boosts today",
-        'q':  {'event': 'boost', 'ts': {"$gte": datetime.utcnow()-timedelta(days=1)}},
-        'proj'  : {'ts':1, 'deck:full_pct': 1},
-        'sort'  : ['ts', -1]
-    },
-
-    'soft-day-list': {
-        'desc': "soft currency today",
-        'q':  {'event': 'cashflow', 'ts': tstoday},
-        'proj': {'type':1, 'amount': 1, 'ltv:soft':1, 'balance': 1 },
-        # 'proj'  : {"player:name":1, 'amount':1 },
-        'sort'  : ['ts', -1]
-    },
     
-    'soft-day-player': {
-        'desc': 'soft spend per player',
+    'test-ag': {
+        'desc': 'aggregation query test',
         'type': 'aggregate',
         'q': {
-            'match': {"event": "cashflow", "currency": "SOFT", 'ts': tstoday },
-            'group': {"_id": "$player:name", "total": {"$sum": "$amount"}}
+            'match': {"event": "cashflow", 'ts': tstoday },
+            'group': {"_id": "$player:name", "total": {"$sum": "$amount"}, "count": {"$sum": 1} }
         }
     }
 
